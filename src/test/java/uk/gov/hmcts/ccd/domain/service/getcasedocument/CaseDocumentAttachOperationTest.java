4a2fe9d0e52ddb3bf21e6b3dc5f023ead223a65b
dharmendra8186 <dharmendra.singh@hmcts.net>
2020-04-16 10:56:07 +0100

    AM-660 : Junit test case for filter document logic.

--- src/test/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/CaseDocumentAttachOperationTest.java
+++ src/test/java/uk/gov/hmcts/ccd/domain/service/getcasedocument/CaseDocumentAttachOperationTest.java
@@ -34,6 +34,8 @@ public class CaseDocumentAttachOperationTest {
     HashMap<String, JsonNode> caseDetailsBefore;
     HashMap<String, JsonNode> caseDataContent;
     CaseDetails caseDetails;
+    Map<String,String> beforeCallBack;
+    CaseDocumentsMetadata  caseDocumentsMetadata;
 
     @BeforeEach
     public void setup() throws IOException {
@@ -89,24 +91,17 @@ public class CaseDocumentAttachOperationTest {
     }
 
     @Test
-    @DisplayName("should  filter the Case Document Meta Data while  2 documents with hashcode coming from request and  2 documents without hash token from callback response")
-    void shouldFilterCaseDocumentMetaData() {
-        Map<String,String> beforeCallBack = new HashMap<>();
-        beforeCallBack.put("b6ee2bff-8244-431f-94ec-9d8ecace8dd6","4d49edc151423fb7b2e1f22d89a2d041b43");
-        beforeCallBack.put("e16f2ae0-d6ce-4bd0-a652-47b3c4d86292","4d49edc151423fb7b2e1f22d87b2d041b34");
+    @DisplayName("should  filter the Case Document Meta Data while  2 documents with hashcode  from request and  2 new documents without hash token from callback response")
+    void shouldFilterCaseDocumentMetaData_With_Scenario_1() {
 
+        prepareInputs();
+        
         Map<String,String> afterCallBack = new HashMap<>();
         afterCallBack.put("320233b8-fb61-4b58-8731-23c83638c9c6",null);
         afterCallBack.put("f5bd63a2-65c5-435e-a972-98ed658ad7d6",null);
         afterCallBack.put("b6ee2bff-8244-431f-94ec-9d8ecace8dd6",null);
         afterCallBack.put("e16f2ae0-d6ce-4bd0-a652-47b3c4d86292",null);
 
-        CaseDocumentsMetadata  caseDocumentsMetadata = CaseDocumentsMetadata.builder()
-            .caseId("12345556")
-            .caseTypeId("BEFTA_CASETYPE_2")
-            .jurisdictionId("BEFTA_JURISDICTION_2")
-            .documents(new ArrayList<>())
-            .build();
         List<DocumentHashToken> expected = Arrays.asList(DocumentHashToken.builder().id("b6ee2bff-8244-431f-94ec-9d8ecace8dd6")
                                            .hashToken("4d49edc151423fb7b2e1f22d89a2d041b43").build(),
                                           DocumentHashToken.builder().id("e16f2ae0-d6ce-4bd0-a652-47b3c4d86292")
@@ -122,12 +117,108 @@ public class CaseDocumentAttachOperationTest {
         );
     }
 
+    @Test
+    @DisplayName("should  filter the Case Document Meta Data while  2 documents with hashcode  from request and  2 new documents with hash token from callback response")
+    void shouldFilterCaseDocumentMetaData_With_Scenario_2() {
+        prepareInputs();
+        Map<String,String> afterCallBack = new HashMap<>();
+        afterCallBack.put("320233b8-fb61-4b58-8731-23c83638c9c6","4d49edc151423fb7b2e1f22d89a2d041b53");
+        afterCallBack.put("f5bd63a2-65c5-435e-a972-98ed658ad7d6","4d49edc151423fb7b2e1f22d89a2d041b63");
+        afterCallBack.put("b6ee2bff-8244-431f-94ec-9d8ecace8dd6",null);
+        afterCallBack.put("e16f2ae0-d6ce-4bd0-a652-47b3c4d86292",null);
+
+        List<DocumentHashToken> expected = Arrays.asList(DocumentHashToken.builder().id("f5bd63a2-65c5-435e-a972-98ed658ad7d6")
+                .hashToken("4d49edc151423fb7b2e1f22d89a2d041b63").build(),
+            DocumentHashToken.builder().id("320233b8-fb61-4b58-8731-23c83638c9c6")
+                .hashToken("4d49edc151423fb7b2e1f22d89a2d041b53").build(),
+            DocumentHashToken.builder().id("b6ee2bff-8244-431f-94ec-9d8ecace8dd6")
+                .hashToken("4d49edc151423fb7b2e1f22d89a2d041b43").build(),
+            DocumentHashToken.builder().id("e16f2ae0-d6ce-4bd0-a652-47b3c4d86292")
+                .hashToken("4d49edc151423fb7b2e1f22d87b2d041b34").build()
+            );
+
+        caseDocumentAttachOperation.filterDocumentFields(caseDocumentsMetadata,beforeCallBack,afterCallBack);
+
+        List<DocumentHashToken> actual=caseDocumentsMetadata.getDocuments();
+
+        assertAll(
+            () -> assertEquals(actual, expected)
+
+        );
+    }
+
+    @Test
+    @DisplayName("should  filter the Case Document Meta Data while  2 documents with hashcode  from request and  replace 1 documents without hash token from callback response")
+    void shouldFilterCaseDocumentMetaData_With_Scenario_3() {
+        prepareInputs();
+
+        Map<String,String> afterCallBack = new HashMap<>();
+        afterCallBack.put("b6ee2bff-8244-431f-94ec-9d8ecace8dd6",null);
+        afterCallBack.put("f5bd63a2-65c5-435e-a972-98ed658ad7d6",null);
+
+        List<DocumentHashToken> expected = Arrays.asList(
+            DocumentHashToken.builder().id("b6ee2bff-8244-431f-94ec-9d8ecace8dd6")
+                .hashToken("4d49edc151423fb7b2e1f22d89a2d041b43").build()
+
+        );
+
+        caseDocumentAttachOperation.filterDocumentFields(caseDocumentsMetadata,beforeCallBack,afterCallBack);
+
+        List<DocumentHashToken> actual=caseDocumentsMetadata.getDocuments();
+
+        assertAll(
+            () -> assertEquals(actual, expected)
+
+        );
+    }
+
+    @Test
+    @DisplayName("should  filter the Case Document Meta Data while  2 documents with hashcode from request and  replace 1 documents with hash token from callback response")
+    void shouldFilterCaseDocumentMetaData_With_Scenario_4() {
+        prepareInputs();
+        Map<String,String> afterCallBack = new HashMap<>();
+        afterCallBack.put("b6ee2bff-8244-431f-94ec-9d8ecace8dd6",null);
+        afterCallBack.put("f5bd63a2-65c5-435e-a972-98ed658ad7d6","4d49edc151423fb7b2e1f22d89a2d041b63");
+
+
+        List<DocumentHashToken> expected = Arrays.asList(
+            DocumentHashToken.builder().id("f5bd63a2-65c5-435e-a972-98ed658ad7d6")
+                .hashToken("4d49edc151423fb7b2e1f22d89a2d041b63").build(),
+            DocumentHashToken.builder().id("b6ee2bff-8244-431f-94ec-9d8ecace8dd6")
+                .hashToken("4d49edc151423fb7b2e1f22d89a2d041b43").build()
+        );
+
+        caseDocumentAttachOperation.filterDocumentFields(caseDocumentsMetadata,beforeCallBack,afterCallBack);
+
+        List<DocumentHashToken> actual=caseDocumentsMetadata.getDocuments();
+
+        assertAll(
+            () -> assertEquals(actual, expected)
+
+        );
+    }
+
+
     static HashMap<String, JsonNode> buildCaseData(String fileName) throws IOException {
         InputStream inputStream =
             CaseDocumentAttachOperationTest.class.getClassLoader().getResourceAsStream("mappings/".concat(fileName));
         return
             new ObjectMapper().readValue(inputStream, new TypeReference<HashMap<String, JsonNode>>() {
             });
+    }
+
+    private void prepareInputs(){
+        beforeCallBack = new HashMap<>();
+        beforeCallBack.put("b6ee2bff-8244-431f-94ec-9d8ecace8dd6","4d49edc151423fb7b2e1f22d89a2d041b43");
+        beforeCallBack.put("e16f2ae0-d6ce-4bd0-a652-47b3c4d86292","4d49edc151423fb7b2e1f22d87b2d041b34");
+
+        caseDocumentsMetadata = CaseDocumentsMetadata.builder()
+            .caseId("12345556")
+            .caseTypeId("BEFTA_CASETYPE_2")
+            .jurisdictionId("BEFTA_JURISDICTION_2")
+            .documents(new ArrayList<>())
+            .build();
+
     }
 }
 
